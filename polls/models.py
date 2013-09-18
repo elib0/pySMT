36a5b01b547337c40a335fa5c2a8f3b80ba6a96d
Eli J. Chavez <skat0@hotmail.com>
2013-09-18 15:22:12 -0430

    Vistas, Urls

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Poll(models.Model):
     def __unicode__(self):
         return self.question
-        
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
