06916ab3accaa93e9c99fdbf054bdc1d9f39918a
Eli J. Chavez <skat0@hotmail.com>
2013-09-25 13:51:43 -0430

    Login Users

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from users import views
 urlpatterns = patterns('',
     url(r'^register/', views.register, name='register'),
     url(r'^save/', views.save, name='save'),
+    url(r'^login/', views.loginuser, name='login'),
 )
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.utils import simplejson
 
 
@@ -23,3 +24,22 @@ def save(request):
             result['message'] = 'Su voto no a sido registrado'
     json = simplejson.dumps(result)
     return HttpResponse(json, mimetype='application/json')
+
+
+def loginuser(request):
+    msj = 'Error Desconocido'
+    if request.method == 'POST':
+        post = request.POST
+        u = authenticate(username=post['name'], password=post['pass'])
+        if u is not None:
+            if u.is_active:
+                login(request, u)
+            else:
+                msj = 'lo sentimos este usuario no se encuntra disponible'
+
+            return redirect('/')
+        else:
+            msj = 'Usuario invalido'
+            redirect('users/login.html', {'msj': msj})
+    else:
+        return render(request, 'users/login.html', {'msj': msj})
