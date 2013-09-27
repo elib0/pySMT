5507fc1b095a91f5dcd3f467a1450627e8ff1c15
Elibo <elijose.c@gmail.com>
2013-09-27 07:43:40 -0430

    Cambios administracion Friendship

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from users.models import Friendship
 
+
 class FriendshipAdmin(admin.ModelAdmin):
-	list_display = ('follower', 'followed')
+    list_display = ('follower', 'followed')
 
 
 admin.site.register(Friendship, FriendshipAdmin)
