d9cc73da98d3fa6fb26c0c445703e26bfd37061a
Elibo <elijose.c@gmail.com>
2013-09-30 18:51:52 -0430

    Arreglos

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from polls.models import Poll, Choice
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
+
 
 class PollAdmin(admin.ModelAdmin):
     # fields = ['pub_date','question']
@@ -14,5 +16,6 @@ class PollAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+
 
 admin.site.register(Poll, PollAdmin)
