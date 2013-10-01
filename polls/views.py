3a978e314810ba36229371db5e27e148fd1dba6e
Elibo <elijose.c@gmail.com>
2013-09-30 22:11:33 -0430

    Some changes

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ context = {'title': ''}
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     context['latest_poll_list'] = latest_poll_list
-    context['title'] = 'Bienvenido a sistema de encstas'
+    context['title'] = 'Bienvenido a sistema de encuestas'
     return render(request, 'index.html', context)
 
 
