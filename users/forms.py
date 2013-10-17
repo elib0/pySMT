a883c39a8ae5237fc8e76b7fb543bf9cfc3af715
Eli J. Chavez <skat0@hotmail.com>
2013-10-17 16:12:01 -0430

    Agregado minimo de caracteres para campo password

--- users/forms.py
+++ users/forms.py
@@ -5,4 +5,4 @@ class LoginForm(forms.Form):
     name = forms.CharField(max_length=30, min_length=5, required=True, label='Nombre')
     password = forms.CharField(widget=forms.PasswordInput(),
                                label='Contrasena',
-                               max_length=30)
+                               max_length=30, min_length=6)
