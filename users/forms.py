3bdf25ce42f42185b5bf9324b74b0611ce97844e
Eli J. Chavez <skat0@hotmail.com>
2013-10-18 08:36:40 -0430

    Added Resgister Form users APP

--- users/forms.py
+++ users/forms.py
@@ -5,4 +5,12 @@ class LoginForm(forms.Form):
     name = forms.CharField(max_length=30, min_length=5, required=True, label='Nombre')
     password = forms.CharField(widget=forms.PasswordInput(),
                                label='Contrasena',
+                               required=True
+                               max_length=30, min_length=6)
+
+class RegisterForm(forms.Form):
+	name = forms.CharField(max_length=30, min_length=5, required=True, label='Nombre')
+	email = forms.CharField(max_length=100, min_length=10, required=True, label='Correo Electronico')
+	password = forms.CharField(widget=forms.PasswordInput(),
+                               label='Contrasena',
                                max_length=30, min_length=6)
